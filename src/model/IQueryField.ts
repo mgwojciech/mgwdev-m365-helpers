2c79174414d7f021d4163f76b728a1ec5e02e149
Peter Pasmik <peter@pasmik.net>
2022-10-20 10:42:23 +0200

    fixed pagination problem, refactor QueryBuilder etc...

--- src/model/IQueryField.ts
+++ src/model/IQueryField.ts
@@ -1,6 +1,29 @@
+export type IQueryFieldType =
+  | 'Text'
+  | 'Number'
+  | 'DateTime'
+  | 'Guid'
+  | 'MultiChoice'
+  | 'Lookup';
+
 export interface IQueryField {
-    name: string,
-    value: string,
-    type?: string,
-    comparer: "Eq" | "Contains"
+  name: string;
+  value: string;
+  type?: IQueryFieldType;
+  comparer:
+    | 'BeginsWith'
+    | 'Contains'
+    | 'DateRangesOverlap'
+    | 'Eq'
+    | 'Geq'
+    | 'Gt'
+    | 'Includes'
+    | 'IsNotNull'
+    | 'IsNull'
+    | 'Leq'
+    | 'Lt'
+    | 'Neq'
+    | 'NotIncludes'
+    | 'Values';
+  includeTimeValue?: boolean;
 }

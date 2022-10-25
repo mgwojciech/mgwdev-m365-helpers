ce78c222368180b6791c51aaf2ea05dccd661fbb
Marcin Wojciechowski <31688852+mgwojciech@users.noreply.github.com>
2022-10-25 08:24:31 +0200

    Revert "fixed pagination problem, refactor QueryBuilder etc..."

--- src/model/IQueryField.ts
+++ src/model/IQueryField.ts
@@ -1,29 +1,6 @@
-export type IQueryFieldType =
-  | 'Text'
-  | 'Number'
-  | 'DateTime'
-  | 'Guid'
-  | 'MultiChoice'
-  | 'Lookup';
-
 export interface IQueryField {
-  name: string;
-  value: string;
-  type?: IQueryFieldType;
-  comparer:
-    | 'BeginsWith'
-    | 'Contains'
-    | 'DateRangesOverlap'
-    | 'Eq'
-    | 'Geq'
-    | 'Gt'
-    | 'Includes'
-    | 'IsNotNull'
-    | 'IsNull'
-    | 'Leq'
-    | 'Lt'
-    | 'Neq'
-    | 'NotIncludes'
-    | 'Values';
-  includeTimeValue?: boolean;
+    name: string,
+    value: string,
+    type?: string,
+    comparer: "Eq" | "Contains"
 }

39ccdf97d2c2a78f75bd817aa998418dd855146d
mgwojciech <m.g.wojciechowski@gmail.com>
2023-04-13 15:32:31 +0200

    added Msal2 to exports

--- src/services/index.ts
+++ src/services/index.ts
@@ -1,4 +1,5 @@
 export * from "./IAuthenticationService";
 export * from "./MsalAuthenticationService";
+export * from "./Msal2AuthenticationService";
 export * from "./cache";
 //export * from "./NodeAppOnlyAuthenticationService";

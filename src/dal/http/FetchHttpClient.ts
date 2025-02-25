3a15ca556440410843c7015ded7f19e2986e4aa6
mgwojciech <m.g.wojciechowski@gmail.com>
2025-02-25 11:20:08 +0100

    added base url for fetch http client

--- src/dal/http/FetchHttpClient.ts
+++ src/dal/http/FetchHttpClient.ts
@@ -1,32 +1,45 @@
 import { IHttpClient, IHttpClientResponse } from "./IHttpClient";
 
 export class FetchHttpClient implements IHttpClient{
+
+    constructor(protected baseUrl?: string){
+        
+    }
+    private buildUrl(url: string){
+        if(url.startsWith("http")){
+            return url;
+        }
+        if(!this.baseUrl){
+            return url;
+        }
+        return this.baseUrl + url;
+    }
     public get(url: string,options?: RequestInit): Promise<IHttpClientResponse> {
-        return fetch(url, {
+        return fetch(this.buildUrl(url), {
             method: "GET",
             ...options
         });
     }
     public post(url: string, options?: RequestInit): Promise<IHttpClientResponse> {
-        return fetch(url, {
+        return fetch(this.buildUrl(url), {
             method: "POST",
             ...options
         });
     }
     public patch(url: string, options?: RequestInit): Promise<IHttpClientResponse> {
-        return fetch(url, {
+        return fetch(this.buildUrl(url), {
             method: "PATCH",
             ...options
         });
     }
     public put(url: string, options?: RequestInit): Promise<IHttpClientResponse> {
-        return fetch(url, {
+        return fetch(this.buildUrl(url), {
             method: "PUT",
             ...options
         });
     }
     public delete(url: string, options?: RequestInit): Promise<IHttpClientResponse> {
-        return fetch(url, {
+        return fetch(this.buildUrl(url), {
             method: "DELETE",
             ...options
         });
